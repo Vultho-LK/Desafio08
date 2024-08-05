675d344677cff776cd187febd9e8c64c9b0a9a4d
Hemerson Luan <140278282+Vultho-LK@users.noreply.github.com>
2024-08-04 21:34:50 -0300

    Add files via upload

---
+++ Desafio08.py
@@ -0,0 +1,3 @@
+# converter metros em cemtimetros e milimetros
+n1 = int(input('digite a quantidade de metros: '))
+print('{} metros equivale a {} cemtrimetros e {} milimetros'.format(n1, n1 * 100, n1 * 1000))
---
+++ README.md
@@ -0,0 +1,3 @@
+Converte metros em cemtimetros e milimetros
+
+<img src=https://github.com/user-attachments/assets/1c54f799-6bd2-4942-9cd9-f47da9301488 width=100>
